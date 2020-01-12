60d71a98c3f695f7026432dd4079c68db35b5d86
tanjunchen <2799194073@qq.com>
2020-01-12 21:39:27 +0800

    add array and slice

---
+++ go-learn/arrayandslice/arrayandslice.go
@@ -0,0 +1,29 @@
+package arrayandslice
+
+func Sum(numbers [] int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumbers := len(numbersToSum)
+	sums = make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+
+	return sums
+}
