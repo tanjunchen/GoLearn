6d31b1cb95e5e6e8681ee4090b44d7f663b421dc
tanjunchen <2799194073@qq.com>
2020-01-12 21:35:18 +0800

    add hello-world

--- go-learn/helloworld/helloworld.go
+++ go-learn/helloworld/helloworld.go
@@ -18,3 +18,24 @@ func Sum(set []int) int {
 	}
 	return result
 }
+
+const englishHelloPrefix = "Hello,"
+
+func Hello(name string) string {
+	return englishHelloPrefix + name
+}
+
+func HelloRefactor(name string) string {
+	return englishHelloPrefix + name
+}
+
+func HelloParameter(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
+
+	return englishHelloPrefix + name
+}
--- go-learn/helloworld/helloworld_test.go
+++ go-learn/helloworld/helloworld_test.go
@@ -20,3 +20,45 @@ func TestSum(t *testing.T) {
 		t.Errorf("Expect %d, but got %d!", expected, actual)
 	}
 }
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("tanjunchen")
+		want := "Hello,tanjunchen"
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	})
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("tanjunchen")
+		want := "Hello,World"
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	})
+}
+
+func TestHelloRefactor(t *testing.T) {
+	// refactor common code
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := HelloRefactor("tanjunchen")
+		want := "Hello,tanjunchen"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := HelloRefactor("World")
+		want := "Hello,World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := HelloParameter("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+}
