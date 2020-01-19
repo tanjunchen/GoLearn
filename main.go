eb20d9d4f4ca31b2ad8179079c37e0ec6319bd03
tanjunchen <2799194073@qq.com>
2020-01-19 11:19:03 +0800

    add go example

--- main.go
+++ main.go
@@ -15,8 +15,32 @@ limitations under the License.
 */
 package main
 
-import "go-learn/cmd"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	cmd.Execute()
+	//cmd.Execute()
+	//rand.Seed(time.Now().UnixNano())
+	//fmt.Println(strconv.Itoa(rand.Intn(10000)))
+	//fmt.Print(rand.New(ran))
+	// 实现思路
+	// 数值转字符串
+	// 逆序循环颠倒字符串
+	// 补足 32 位
+	// 字符串转数值
+	num := 00000010100101000001111010011100
+	str := strconv.FormatUint(uint64(num), 2)
+	rev := ""
+	for i := len(str) - 1; i >= 0; i-- {
+		rev = rev + str[i:i+1]
+	}
+	if len(rev) < 32 {
+		rev = rev + strings.Repeat("0", 32-len(rev))
+	}
+	n, _ := strconv.ParseUint(rev, 2, 64)
+	//
+	fmt.Print(uint32(n))
 }
